149f527beccaf477b566e3c698b6fd7303cbc68c
Austin Brock <austinjbrock@gmail.com>
2022-09-29 21:50:37 -0700

    added dom rendering to inject templates

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 fetch('https://apis.scrimba.com/jsonplaceholder/posts',{method: "GET"})
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+   //name our data reference
+   let postArray = data.slice(0,8)
+   //name where we will put the data
+   let container = document.getElementById('blog-list')
+   //name a variable we can update
+   let postHtml = ''
 
+   //loop over the list of posts until we hit our desired numbers
+   for (const post of postArray) {
+      //update our variable with the html tags we want to create and pass in our content
+      postHtml += `
+         <h2 class="title post">${post.title}</h2>
+         <p class="body" post>${post.body}</p>
+         <hr />
+         `
+   }
+
+   // inject our new data into our page
+   container.innerHTML = postHtml
+
+})
